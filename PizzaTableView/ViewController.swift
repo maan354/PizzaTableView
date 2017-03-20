20c3d4eb4b590d3e6911d4f0df6dce7e3167c825
Achraf Mahha <mahha@mbp-de-mahha.boiscolombes.fr.ibm.com>
2017-03-20 12:33:59 +0100

    Initial Commit

---
+++ PizzaTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaTableView
+//
+//  Created by MAHHA on 20/03/2017.
+//  Copyright © 2017 MAHHA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
